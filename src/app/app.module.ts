a11308c69e2862a948f713f7fc42a408cf2db4df
Kanto07 <raoelisonkanto@gmail.com>
2022-04-03 00:16:23 +0200

    deuxieme commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { MatiereComponent } from './matiere/matiere.component';
+import { NoteComponent } from './note/note.component';
+import { MoyenneComponent } from './moyenne/moyenne.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ResultatComponent } from './resultat/resultat.component';
+
+export const ROUTES: Routes=[
+  {path: 'Etudiant', component: EtudiantComponent},
+  {path: 'Matiere', component: MatiereComponent},
+  {path: 'Note', component: NoteComponent},
+  {path: 'Moyenne', component: MoyenneComponent},
+  {path: 'Resultat', component: ResultatComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EtudiantComponent,
+    MatiereComponent,
+    NoteComponent,
+    MoyenneComponent,
+    HeaderComponent,
+    ResultatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
